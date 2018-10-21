dcc0d4271731bf04ec463a06b6f5d5a4fc9afa80
Johan Hubau <johan.hubau@gmail.com>
2018-10-21 12:13:52 +0200

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	new file:   main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+import random as rd
+
+#Colors are stored in this array, you can add or removes colors to your preference:
+colors = ['fff9c7','38999c','52bec2','fefdef','fad3cf','a797c8','2776a5','1d1d1b','c50a66','edc613','ef790e','fff9c7','2c2457','223b6f','de7778','f3f0ef','11334c',
+'155894','f5f6e7','ea6215','de0b3c','e4d2d5','2089a8','164e5a','3097bb','f9f5c2','f1b876','d65969','263579','306ab2','3cbee9','bee3ef','f6a119','29a13d','26a8ac',
+'2487c8','1fa1c3','f3c418','77b86d','ee713d','3b2257','bc0e74','eb5b2b','fae614','fab734','ee7338','67c3cc']
